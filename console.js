a4e1a9b57290068853ecbbf48609ed9dbb71ab6e
Nitin Santosh Gavhane <51412436+NitinGavhane@users.noreply.github.com>
2024-10-24 14:22:30 +0530

    Done Some changes Update console.js

--- console.js
+++ console.js
@@ -26,53 +26,76 @@ THE SOFTWARE.
  * This code is a simplified and unjQuerified version of Typerjs by Sam Phippen, 
  * with the cursor modification by @nitin
  */
-const Typer ={
+const Typer = {
     text: null,
-	accessCountimer:null,
-	index:0, // current cursor position
-	speed:2, // speed of the Typer
-	file:"", //file, must be setted
-	finish:false,
-   
-    init: function(){// inizialize Hacker Typer
-	
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", Typer.file);
-        xhr.onreadystatechange = function(data) {
-            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {         
-                Typer.text=xhr.response;
-            }
-        }
-        xhr.send();
+	accessCountimer: null,
+	index: 0, // current cursor position
+	speed: 2, // speed of the Typer
+	file: "", // file, must be set
+	finish: false,
+	paragraphs: [], // Store each paragraph separately
+	currentParagraphIndex: 0, // Track the current paragraph being displayed
+
+	init: function() { // initialize Hacker Typer
+		const xhr = new XMLHttpRequest();
+		xhr.open("GET", Typer.file);
+		xhr.onreadystatechange = function(data) {
+			if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+				// Split the content into paragraphs and store
+				Typer.paragraphs = xhr.response.split('\n\n'); // Assuming paragraphs are separated by double newlines
+				Typer.text = Typer.paragraphs[Typer.currentParagraphIndex]; // Start with the first paragraph
+			}
+		}
+		xhr.send();
 	},
 
+	addText: function() {
+		if (Typer.text) {
+			Typer.index += Typer.speed;
+			let text = Typer.text.substring(0, Typer.index);
+			const rtn = new RegExp("\n", "g");
+			document.getElementById("console").innerHTML = text.replace(rtn, "<br/>");
+			window.scrollBy(0, 50);
+		}
+	},
 
-    addText: function(){
-        if(Typer.text){
-            Typer.index+=Typer.speed;
-            let text=Typer.text.substring(0,Typer.index);
-			const rtn= new RegExp("\n", "g");
-            document.getElementById("console").innerHTML = text.replace(rtn,"<br/>");
-            window.scrollBy(0,50); 
-        }
-    },
+	blink: function() {
+		var cursor = document.getElementById("cursor");
+		cursor.classList.toggle("vis");
+	},
 
-    blink : function(){
-        var cursor = document.getElementById("cursor");
-        cursor.classList.toggle("vis");
-    }
+	// Function to move to the next paragraph when "Enter" is pressed
+	nextParagraph: function() {
+		if (Typer.currentParagraphIndex < Typer.paragraphs.length - 1) {
+			// Move to the next paragraph
+			Typer.currentParagraphIndex++;
+			Typer.index = 0; // Reset the index
+			Typer.text = Typer.paragraphs[Typer.currentParagraphIndex];
+		} else {
+			// All paragraphs finished
+			clearInterval(timer);
+			clearInterval(Typer.accessCountimer); // Stop cursor blinking
+		}
+	}
 }
 
-Typer.speed=2;
-Typer.file="data.bio";
+Typer.speed = 2;
+Typer.file = "data.bio";
 Typer.init();
 
-var timer = setInterval("t();", 30);
+var timer = setInterval(function() { t(); }, 30);
 function t() {
 	Typer.addText();
 	if (Typer.text && Typer.index > Typer.text.length) {
 		clearInterval(timer);
-		// inizialize timer for blinking cursor
-		Typer.accessCountimer=setInterval(function(){Typer.updLstChr();},500);
+		Typer.accessCountimer = setInterval(function() { Typer.blink(); }, 500);
 	}
 }
+
+// Listen for Enter key press to skip Typer effect and move to the next paragraph
+document.addEventListener("keydown", function(event) {
+	if (event.key === "Enter") {
+		Typer.nextParagraph();
+	}
+});
+
