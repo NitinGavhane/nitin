38fb5116331fb7ab86462885c7c24dc299e2685b
Nitin Santosh Gavhane <51412436+NitinGavhane@users.noreply.github.com>
2024-10-24 14:29:33 +0530

    Update console.js

--- console.js
+++ console.js
@@ -29,7 +29,7 @@ THE SOFTWARE.
 const Typer = {
     text: null,
     index: 0, // current cursor position
-    speed: 4, // speed of the Typer
+    speed: 2, // speed of the Typer
     file: "", // file, must be set
     timer: null, // stores the typing timer
     finish: false,
@@ -67,7 +67,7 @@ const Typer = {
     }
 };
 
-Typer.speed = 2;
+Typer.speed = 4;
 Typer.file = "data.bio";
 Typer.init();
 
